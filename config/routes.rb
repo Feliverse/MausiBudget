970f7ad4e93f7a093c786b9b2fa6dcd35b3d4c8c
Feliverse <114632980+Feliverse@users.noreply.github.com>
2023-08-08 16:06:07 -0400

    config splash page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
+  root to: 'splash#index'
+  
   devise_for :users
+  devise_scope :user do
+    get '/users/sign_up', to: 'devise/registrations#new', as: :new_user_registration
+  end
+  
+  authenticated :user do
+    root to: 'groups#index', as: :authenticated_root
+  end
+  
   resources :categories
   resources :entities
   resources :groups
   resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
